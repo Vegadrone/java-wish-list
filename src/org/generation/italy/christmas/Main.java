f71afc8d3b4af188de4a7ba94e2319a05a2fce02
Vegadrone <spenaantonino@gmail.com>
2022-11-23 12:33:00 +0100

    ex complete

--- src/org/generation/italy/christmas/Main.java
+++ src/org/generation/italy/christmas/Main.java
@@ -22,16 +22,25 @@ public class Main {
 			System.out.println("Inserisci un nuovo desiderio nella lista: ");
 			String newWish = sc.next();
 			wishList.add(newWish);
+			int wishLen = wishList.size();
+			System.out.println("Ora hai: " + wishLen + " desideri!" );
 			
 			System.out.println("Vuoi fermarti qui? Digita: 's' o 'n' ");
 			String userChoice = sc.next();
 			
 			if (userChoice.equals("s")) {
 				compile =true;
+				wishList.sort(null);
 			}
 		}
 		
+		System.out.println("La tua lista contiene: ");
 		
-		System.out.println(wishList);
+		int counter = 0;
+	
+		for(String wish : wishList ) {
+			System.out.println((counter += 1) + "." + " " + wish.substring(0,1).toUpperCase() + wish.substring(1).toLowerCase());
+		}
+		
 	}
 }
