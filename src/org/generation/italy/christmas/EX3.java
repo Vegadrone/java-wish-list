4e1a830558d0f5a80013afd72f1e80b6ceec5b8b
Vegadrone <spenaantonino@gmail.com>
2022-11-23 17:12:51 +0100

    EX3 Complete

--- src/org/generation/italy/christmas/EX3.java
+++ src/org/generation/italy/christmas/EX3.java
@@ -1,29 +1,38 @@
 package org.generation.italy.christmas;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.TreeMap;
 
 public class EX3 {
 		 public static void main(String[] args) {
 			 	
 			 Scanner sc = new Scanner(System.in);
-				
-			 Set <String> userStringLetters = new HashSet<>();
-				
 			 System.out.println("Inserisci una parola");
-				
-			 String userWord= sc.next();
-			 
+			 String userWord = sc.next();
 			 sc.close();
 			 
-			 String [] word = userWord.split("");
+			 String [] wordLetters = userWord.split("");
+			
+			 Map<String, Integer> wordMap = new TreeMap<>();
+			 
+			 for(String singleLetter : wordLetters) {
+				 
+				 int counter = 0;
+				 
+				 if (!wordMap.containsKey(singleLetter)) {
+						counter = 1;
+						wordMap.put(singleLetter, counter);
+				 } else {
+						counter = wordMap.get(singleLetter) + 1;
+						wordMap.put(singleLetter, counter);
+					}
+			 }
 				
-			 for (int i = 0; i < word.length; i++) {
-				String singleLetter = word[i];
-				userStringLetters.add(singleLetter);
-				}
-			 System.out.println(userStringLetters);
-		 	}
+			 System.out.println(wordMap);
+			
 	}
 }
